881bdd4053f3ea840e477699ea9504fe1caac378
Kirill Gladkikh <k.gladkikh@mail.ru>
2026-02-19 12:28:59 +0300

    CRUD for Course, Lesson init

--- lms/views.py
+++ lms/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets, generics
+from lms.models import Course, Lesson
+from lms.serializers import CourseSerializer, LessonSerializer
 
 
+# CRUD для модели Course с использованием ViewSet
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+# Получение списка уроков и создание нового
+class LessonListCreateView(generics.ListCreateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+# Получение, обновление и удаление конкретного урока
+class LessonRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
