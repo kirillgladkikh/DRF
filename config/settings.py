59592a74e43d18a20ccab4019f3b833fa22f3521
kirill.gladkikh <k.gladkikh@mail.ru>
2026-02-25 13:21:10 +0300

    config/settings.py STATICFILES_DIRS commented

--- config/settings.py
+++ config/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]  # Квадратные скобки обязательны, так как это список путей.
+# STATICFILES_DIRS = [BASE_DIR / "static"]  # Квадратные скобки обязательны, так как это список путей.
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(
