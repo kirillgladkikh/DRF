e188704ac3e473da5e9cf67aa10421e75090d170
kirill.gladkikh <k.gladkikh@mail.ru>
2026-03-01 15:19:55 +0300

    djangorestframework-simplejwt init

--- config/settings.py
+++ config/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    
+
     "rest_framework",
     "django_filters",
     'rest_framework_simplejwt',
@@ -161,6 +161,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
 }
 
 # Настройки срока действия токенов
