127d62a3686f2ba4a5f4fe1db980b863a6bdfdd0
kirill.gladkikh <k.gladkikh@mail.ru>
2026-03-01 15:24:27 +0300

    djangorestframework-simplejwt init

--- config/settings.py
+++ config/settings.py
@@ -164,7 +164,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',  # 'rest_framework.permissions.IsAuthenticated',
     ]
-AllowAny
 }
 
 # Настройки срока действия токенов
