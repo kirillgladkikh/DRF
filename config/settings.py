33067f115f431670f175312d165a42e3e04fccca
Kirill Gladkikh <k.gladkikh@mail.ru>
2026-03-12 16:28:22 +0300

    Задание 1. Настройте проект для работы с Celery. Также настройте приложение на работу с celery-beat для выполнения периодических задач. Не забудьте вынести настройки Redis в переменные окружения.

--- config/settings.py
+++ config/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     "django_filters",
     "rest_framework_simplejwt",
     "drf_yasg",
+    "django_celery_beat",
     # СЮДА ВВОДИ ИМЕНА СВОИХ ПРИЛОЖЕНИЙ
     "lms",
     "users",  # ВАЖНО: ПРИНЯТО ИМЕННО users!, а не user.
@@ -198,5 +199,22 @@ SIMPLE_JWT = {
 #
 # if CACHE_ENABLED:
 #     CACHES = {
-#         "default": {"BACKEND": "django.core.cache.backends.redis.RedisCache", "LOCATION": "redis://localhost:6379"}
+#         "default": {"BACKEND": "django.core.cache.backends.redis.RedisCache", "LOCATION": REDIS_LOCATION}
 #     }
+
+# Настройки для Celery
+
+# URL-адрес брокера сообщений
+CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL") # Например, Redis, который по умолчанию работает на порту 6379
+
+# URL-адрес брокера результатов, также Redis
+CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND")
+
+# Часовой пояс для работы Celery
+CELERY_TIMEZONE = "Europe/Moscow"
+
+# Флаг отслеживания выполнения задач
+CELERY_TASK_TRACK_STARTED = True
+
+# Максимальное время на выполнение задачи
+CELERY_TASK_TIME_LIMIT = 30 * 60
